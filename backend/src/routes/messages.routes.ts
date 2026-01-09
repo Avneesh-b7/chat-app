b7283164270f9213ae3c1836cf44256982b89855
avn7 <avnbha7@gmail.com>
2026-01-09 14:46:59 +0530

    starting afresh ...

--- backend/src/routes/messages.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sendMessageController } from "../controllers/messages.controllers.ts";
-
-const messagesRouter = Router();
-
-messagesRouter.post("/send", sendMessageController);
-
-export { messagesRouter };
